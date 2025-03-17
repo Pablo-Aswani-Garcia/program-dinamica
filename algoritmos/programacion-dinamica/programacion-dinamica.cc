1abdb64190740b2b18e789185b37ab39564fc195
Pablo-Aswani-Garcia <alu0101546001@ull.edu.es>
2025-03-17 11:08:00 +0100

    documentación

--- algoritmos/programacion-dinamica/programacion-dinamica.cc
+++ algoritmos/programacion-dinamica/programacion-dinamica.cc
@@ -32,11 +32,15 @@ std::pair<std::vector<std::string>, int> ProgramacionDinamica::resolver(
   // 0001 -> Ciudad 1
   // 0011 -> Ciudad 1 y 2
   // 0101 -> Ciudad 1 y 3
+  // se iteran sobre las 2 ^ num_ciudades - 1 ciudades	
   for (int mask = 1; mask < (1 << num_ciudades); ++mask) {
+    // se itera sobre las ciudades
     for (int ciudad = 0; ciudad < num_ciudades; ++ciudad) {
-      if (mask & (1 << ciudad)) { // Si la ciudad ya fue visitada
+      // Si la ciudad ya fue visitada
+      if (mask & (1 << ciudad)) { 
         for (int ciudad_destino = 0; ciudad_destino < num_ciudades; ++ciudad_destino) {
-          if (!(mask & (1 << ciudad_destino))) { // Si la ciudad no ha sido visitada
+          // Si la ciudad destino no ha sido visitada
+          if (!(mask & (1 << ciudad_destino))) { 
             int nueva_mask = mask | (1 << ciudad_destino);
             if (tabla_memoizacion[mask][ciudad] != INT_MAX) { // Evitar suma con INT_MAX
               tabla_memoizacion[nueva_mask][ciudad_destino] = std::min(
@@ -52,7 +56,8 @@ std::pair<std::vector<std::string>, int> ProgramacionDinamica::resolver(
   // Cálculo del costo mínimo regresando a la ciudad inicial
   int costo_minimo = INT_MAX;
   int ultima_ciudad = -1;
-  
+  // se tiene en cuenta que la ciudad 0 es la ciudad de inicio
+  // y debe ser la última ciudad a visitar
   for (int i = 1; i < num_ciudades; ++i) {
     if (tabla_memoizacion[VISITED_ALL][i] != INT_MAX) { // Evita sumar INT_MAX
       int costo_actual = tabla_memoizacion[VISITED_ALL][i] + transiciones[i][0];
@@ -64,7 +69,7 @@ std::pair<std::vector<std::string>, int> ProgramacionDinamica::resolver(
   }
   
   
-  // Reconstrucción de la mejor ruta
+  // Reconstrucción de la mejor ruta(+1 para la ciudad de inicio)
   std::vector<std::string> ruta(num_ciudades + 1);
   int mask = VISITED_ALL;
   int pos = ultima_ciudad;
@@ -74,18 +79,20 @@ std::pair<std::vector<std::string>, int> ProgramacionDinamica::resolver(
     int next_pos = -1;
     
     for (int j = 0; j < num_ciudades; ++j) {
+      // si j esta en mask y el costo de llegar a pos desde j es igual al costo de llegar a pos desde j
       if ((mask & (1 << j)) && tabla_memoizacion[mask][pos] != INT_MAX &&
           tabla_memoizacion[mask][pos] == tabla_memoizacion[mask ^ (1 << pos)][j] + transiciones[j][pos]) {
         next_pos = j;
         break;
       }
     }
-  
+    // se hace un XOR para eliminar la ciudad visitada
+    // de la ruta
     mask ^= (1 << pos);
     pos = next_pos;
   }
-  
-  ruta[num_ciudades] = ciudades[0]; // Volver al inicio
+  // se añade a la ruta la ciudad de inicio al final
+  ruta[num_ciudades] = ciudades[0]; 
   
   return {ruta, costo_minimo};
 }
